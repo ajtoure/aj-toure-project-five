7c2cbfa50a201fb501a53f5d98ce5fb2f3020417
ajtoure <assiyahjamilla@gmail.com>
2020-08-12 16:32:01 -0400

    Updates

---
+++ precache-manifest.b95c4954903ce3b5f0987467ea24a1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd7bcf4200124b4d2f6dea9cf206875",
+    "url": "/aj-toure-project5/index.html"
+  },
+  {
+    "revision": "4e18a404d2f9773a27d7",
+    "url": "/aj-toure-project5/static/css/main.517aff5d.chunk.css"
+  },
+  {
+    "revision": "d23d7a5d5594b6afb9da",
+    "url": "/aj-toure-project5/static/js/2.866709cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aj-toure-project5/static/js/2.866709cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e18a404d2f9773a27d7",
+    "url": "/aj-toure-project5/static/js/main.4f744ead.chunk.js"
+  },
+  {
+    "revision": "7abf760dd76b45b2cc2f",
+    "url": "/aj-toure-project5/static/js/runtime-main.100e1d8d.js"
+  }
+]);
